98f3d340f10a1271b1707f29bd51be5c9f5099e3
HaloNamaSayaLuqman272 <mluqman272@gmail.com>
2026-09-25 06:53:51 +0700

    add new feature api api.go and update package

---
+++ api/api.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"sistem_kasir_dan_database_toko/api/middlewares"
+	"sistem_kasir_dan_database_toko/auth"
+	"sistem_kasir_dan_database_toko/categories"
+	"sistem_kasir_dan_database_toko/package/fileupload"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/labstack/echo/v5"
+	"gorm.io/gorm"
+)
+
+func NewEcho(repository *gorm.DB, cld *cloudinary.Cloudinary, jwtConfig middlewares.JWTConfig) *echo.Echo {
+	var (
+		e               = echo.New()
+		authService     = auth.New(repository)
+		categoryService = categories.New(repository)
+		uploader        = &fileupload.CloudinaryUploader{Cld: cld}
+	)
+}
