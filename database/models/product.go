dde0ec917b4da3355a9694deb0fc3b363ab140ba
HaloNamaSayaLuqman272 <mluqman272@gmail.com>
2026-09-25 20:38:28 +0700

    update data type UpdatedAt and DeletedAt on models product

--- database/models/product.go
+++ database/models/product.go
@@ -7,19 +7,19 @@ import (
 )
 
 type Product struct {
-	ID          uint           `json:"id" gorm:"primaryKey"`
-	NameProduct string         `json:"name_product"`
-	CategoryID  uint           `json:"category_id"`
-	Category    Category       `json:"category"`
-	Description string         `json:"description"`
-	Company     string         `json:"company"`
-	Barcode     string         `json:"barcode"`
-	Weight      uint           `json:"weight"`
-	ExpiredDate string         `json:"expired_date"`
-	Price       float64        `json:"price"`
-	Stock       uint           `json:"stock"`
-	ImageLink   string         `json:"image_link"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	ID          uint            `json:"id" gorm:"primaryKey"`
+	NameProduct string          `json:"name_product"`
+	CategoryID  uint            `json:"category_id"`
+	Category    Category        `json:"category"`
+	Description string          `json:"description"`
+	Company     string          `json:"company"`
+	Barcode     string          `json:"barcode"`
+	Weight      uint            `json:"weight"`
+	ExpiredDate string          `json:"expired_date"`
+	Price       float64         `json:"price"`
+	Stock       uint            `json:"stock"`
+	ImageLink   string          `json:"image_link"`
+	CreatedAt   time.Time       `json:"created_at"`
+	UpdatedAt   *time.Time      `json:"updated_at"`
+	DeletedAt   *gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
